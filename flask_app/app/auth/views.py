b820136797f3abe156449ff74ad510256499202b
felixphan9 <phuc.phsn2001@gmail.com>
2025-03-10 08:36:59 +0700

    If you are not confirmed, you are not allowed :)

--- flask_app/app/auth/views.py
+++ flask_app/app/auth/views.py
@@ -1,6 +1,10 @@
 from flask import render_template, redirect, url_for, flash, request
 from flask_login import login_user, login_required, logout_user
 from . import auth
+<<<<<<< HEAD
+=======
+from flask_login import current_user
+>>>>>>> If you are not confirmed, you are not allowed :)
 from ..models import User
 from .forms import LoginForm, RegistrationForm
 from .. import db
@@ -8,6 +12,20 @@ from flask_mail import Message
 from flask import current_app
 from app import mail
 
+<<<<<<< HEAD
+=======
+@auth.before_app_request
+def before_request():
+    '''This handler will intercept a request when three conditions are met:
+    1. The request is not for a static file (the URL starts with /static/).
+    2. The user is authenticated (logged in).
+    3. The user has not confirmed their account yet.
+    If these conditions are met, the user is redirected to the unconfirmed page.'''  
+    if current_user.is_authenticated and not current_user.confirmed \
+            and request.blueprint != 'auth' and request.endpoint != 'static':
+        return redirect(url_for('auth.unconfirmed'))
+    
+>>>>>>> If you are not confirmed, you are not allowed :)
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -83,4 +101,36 @@ def confirm_email(token):
 def logout():
     logout_user()
     flash('You have been logged out.')
+<<<<<<< HEAD
     return redirect(url_for('main.home'))
+=======
+    return redirect(url_for('main.home'))
+
+@auth.route('/resend_confirmation')
+@login_required
+def resend_confirmation():
+    token = current_user.get_token(expires_in=3600)
+    print('token' , token)
+    confirm_url = url_for('auth.confirm_email', token=token, _external=True)
+    subject = "Reconfirm your email"
+    body = f"Hi {current_user.username},\n\nPlease click the link below to re-confirm your email address:\n{confirm_url}\n\nThank you!"
+    
+    msg = Message(subject=subject,
+                  sender=current_app.config['MAIL_DEFAULT_SENDER'],
+                  recipients=[current_user.email],
+                  body=body)
+    try:
+        mail.send(msg)
+        flash('A new confirmation email has been sent to you by email.', 'info')
+    except Exception as e:
+        flash(f'An error occurred while sending the email: {e}', 'danger')
+    
+    return redirect(url_for('main.home'))
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous or current_user.confirmed:
+        return redirect(url_for('main.home'))
+    
+    return render_template('auth/unconfirmed.html')
+>>>>>>> If you are not confirmed, you are not allowed :)
